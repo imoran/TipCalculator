a8f851124ccebaa6c13abd903e49ef876eb18db2
imoran <imoran@mail.ccsf.edu>
2015-12-13 21:11:40 -0800

    More edits

--- Tips/SettingsViewController.swift
+++ Tips/SettingsViewController.swift
@@ -9,27 +9,101 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
+    
+    @IBOutlet weak var segControl: UISegmentedControl!
+    @IBOutlet weak var textField: UITextField!
+    
+    var lowestTip: Float!
+    var midTip: Float!
+    var highestTip: Float!
 
+    let userDefaults = NSUserDefaults.standardUserDefaults()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        lowestTip = userDefaults.floatForKey("lowest_tip")
+        midTip = userDefaults.floatForKey("mid_tip")
+        highestTip = userDefaults.floatForKey("highest_tip")
+        
+        segControl.setTitle("\(Int(lowestTip * 100))%", forSegmentAtIndex: 0)
+        segControl.setTitle("\(Int(midTip * 100))%", forSegmentAtIndex: 1)
+        segControl.setTitle("\(Int(highestTip * 100))%", forSegmentAtIndex: 2)
+        
     }
     
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func segControlTapped(sender: AnyObject) {
+        print(segControl.selectedSegmentIndex)
+    }
+    
+    
+    @IBAction func updateSegment(sender: AnyObject) {
+        switch segControl.selectedSegmentIndex {
+        case 0:
+            let newValue = NSString(string: textField.text!).floatValue
+            segControl.setTitle(String(newValue), forSegmentAtIndex: segControl.selectedSegmentIndex)
+            
+            userDefaults.setFloat((newValue / 100.00), forKey: "lowest_tip")
+            
+        case 1:
+           segControl.setTitle(textField.text, forSegmentAtIndex: 1)
+           let newValueOne = NSString(string: textField.text!).floatValue
+           segControl.setTitle(String(newValueOne), forSegmentAtIndex: segControl.selectedSegmentIndex)
+           userDefaults.setFloat((newValueOne / 100.00), forKey: "mid_tip")
+           
+        case 2:
+            segControl.setTitle(textField.text, forSegmentAtIndex: 2)
+            let newValueTwo = NSString(string: textField.text!).floatValue
+            segControl.setTitle(String(newValueTwo), forSegmentAtIndex: segControl.selectedSegmentIndex)
+            userDefaults.setFloat((newValueTwo / 100.00), forKey: "highest_tip")
+            
+        default:
+            print("something didn't work here...")
+        }
+        
+        textField.text = ""
+    }
+    
+    @IBAction func onDefaultChanged(sender: AnyObject) {
+        
+        let userDefaults = NSUserDefaults.standardUserDefaults()
+        
+        lowestTip = userDefaults.floatForKey("lowest_tip")
+        midTip = userDefaults.floatForKey("mid_tip")
+        highestTip = userDefaults.floatForKey("highest_tip")
+        
+        
+        if (segControl.selectedSegmentIndex == 0) {
+            userDefaults.setFloat(lowestTip, forKey: "default_tip")
+            userDefaults.setInteger(0, forKey: "default_index")
+            userDefaults.setBool(true, forKey: "default_tip_changed")
+        }else if (segControl.selectedSegmentIndex == 1) {
+            userDefaults.setFloat(midTip, forKey: "default_tip")
+            userDefaults.setInteger(1, forKey: "default_index")
+            userDefaults.setBool(true, forKey: "default_tip_changed")
+        }else if (segControl.selectedSegmentIndex == 2) {
+            userDefaults.setFloat(highestTip, forKey: "default_tip")
+            userDefaults.setInteger(2, forKey: "default_index")
+            userDefaults.setBool(true, forKey: "default_tip_changed")
 
+        }
+        userDefaults.synchronize()
+    }
+    
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
--- Tips/ViewController.swift
+++ Tips/ViewController.swift
@@ -7,53 +7,164 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController
-   {
-
+    {
+    
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var twoPeopleLabel: UILabel!
+    @IBOutlet weak var threePeopleLabel: UILabel!
+    @IBOutlet weak var fourPeopleLabel: UILabel!
+    
+    var tipPercentage: Float!
+    let defaults = NSUserDefaults.standardUserDefaults()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        tipLabel.text = "$0.00"
-        totalLabel.text = "$0.00"
+        
+        billField.text = String(defaults.floatForKey("savedAmt"))
+        defaults.synchronize()
+    
+    
+        let now = NSDate.timeIntervalSinceReferenceDate()
+        let tenMinute = 10.0 * 60.00
+    
+        let currencyFormatter = NSNumberFormatter()
+        currencyFormatter.numberStyle = NSNumberFormatterStyle.CurrencyStyle
+        currencyFormatter.locale = NSLocale.currentLocale()
+    
+        if ((now - defaults.doubleForKey("previous_bill_time")) < tenMinute) {
+            billField.text = String(defaults.floatForKey("savedAmt"))
+            calculateTip(defaults.floatForKey("default_tip"))
+        
+        } else {
+            tipLabel.text = currencyFormatter.stringFromNumber(0.00)
+            totalLabel.text = currencyFormatter.stringFromNumber(0.00)
+            twoPeopleLabel.text = currencyFormatter.stringFromNumber(0.00)
+            threePeopleLabel.text = currencyFormatter.stringFromNumber(0.00)
+        
+            tipPercentage = defaults.floatForKey("default_tip")
+        }
     
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
-        // Dispose of any resources that can be recreated.
     }
-
-    func onEditingChanged(sender: AnyObject) {
+    
+    @IBAction func onEditingChanged(sender: AnyObject) {
         
+        let tipPercentages = [defaults.floatForKey("lowest_tip"), defaults.floatForKey("mid_tip"), defaults.floatForKey("highest_tip")]
+        tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        calculateTip(tipPercentage)
         
-        var tipPercentages = [0.18, 0.2, 0.22]
-        let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
-        
-        let billAmount = NSString(string:billField.text!).doubleValue
+        let billAmount = NSString(string:billField.text!).floatValue
         let tip = billAmount * tipPercentage
         let total = billAmount + tip
+    
         
-        NSUserDefaults.standardUserDefaults().setObject("billAmount", forKey: billAmount)
-        NSUserDefaults.standardUserDefaults().synchronize()
-        print(NSUserDefaults.standardUserDefaults().objectForKey("myBill")!)
+        let currencyFormatter = NSNumberFormatter()
+        currencyFormatter.numberStyle = NSNumberFormatterStyle.CurrencyStyle
+        currencyFormatter.locale = NSLocale.currentLocale()
+        calculateTip(tipPercentage)
         
-        tipLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
+        defaults.setDouble(NSDate.timeIntervalSinceReferenceDate(), forKey: "previous_bill_time")
         
-        
+        tipLabel.text = currencyFormatter.stringFromNumber(tip)!
+        totalLabel.text = currencyFormatter.stringFromNumber(total)!
         
     }
-
-    @IBAction func onTap(sender: AnyObject)
-    {
+    
+    @IBAction func onTap(sender: AnyObject) {
         view.endEditing(true)
-       
-}
+        
+    }
+    
+    
+    @IBAction func onBillAmount(sender: AnyObject) {
+        calculateTip(tipPercentage)
+    }
+    
+    
+    override func viewWillAppear(animated:Bool) {
+        super.viewWillAppear(animated)
+        print("View will appear")
+        billField.becomeFirstResponder()
+        
+        if (defaults.boolForKey("default_tip_changed")) {
+            tipControl.selectedSegmentIndex = defaults.integerForKey("default_index")
+            updateTipDisplay()
+            calculateTip(defaults.floatForKey("default_tip"))
+            defaults.setBool(false, forKey: "default_tip_changed")
+        }
+    
+    }
+    
+    override func viewDidAppear(animated:Bool) {
+            super.viewWillAppear(animated)
+            print("View did appear")
+//            
+//            var tipPercentages = [0.18, 0.2, 0.22]
+//            let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+//            let billAmount = NSString(string:billField.text!).floatValue
+//            let tip = billAmount * tipPercentage
+//            let total = billAmount + tip
+//            
+//            //tipLabel.text = String(format: "$%.2f", tip)
+//            //totalLabel.text = String(format: "$%.2f", total)
+//            
+//            let currencyFormatter = NSNumberFormatter()
+//            currencyFormatter.numberStyle = NSNumberFormatterStyle.CurrencyStyle
+//            currencyFormatter.locale = NSLocale.currentLocale()
+//            
+//            tipLabel.text = currencyFormatter.stringFromNumber(tip)!
+//            totalLabel.text = currencyFormatter.stringFromNumber(total)!
+    }
+    
+    override func viewWillDisappear(animated:Bool) {
+            super.viewWillDisappear(animated)
+            print("View will disappear")
+            let myText = billField.text
+            NSUserDefaults.standardUserDefaults().setObject(myText, forKey: "savedAmt")
+            
+            
+    }
+    override func viewDidDisappear(animated:Bool) {
+            super.viewDidDisappear(animated)
+            print("View did disappear")
+    }
+    
+    func calculateTip(tipPercentage: Float) {
+        let billAmount = NSString(string:billField.text!).floatValue
+        defaults.setFloat(billAmount, forKey: "savedAmt")
+        let tip = billAmount * tipPercentage
+        let total = billAmount + tip
+        let twoPeopleLabelBill = total / 2.0
+        let threePeopleLabelBill = total / 3.0
+        let fourPeopleLabelBill = total / 4.0
+        
+        let currencyFormatter = NSNumberFormatter()
+        currencyFormatter.numberStyle = NSNumberFormatterStyle.CurrencyStyle
+        currencyFormatter.locale = NSLocale.currentLocale()
+        
+        tipLabel.text = currencyFormatter.stringFromNumber(tip)!
+        totalLabel.text = currencyFormatter.stringFromNumber(total)!
+        twoPeopleLabel.text = currencyFormatter.stringFromNumber(twoPeopleLabelBill)!
+        threePeopleLabel.text = currencyFormatter.stringFromNumber(threePeopleLabelBill)!
+        fourPeopleLabel.text = currencyFormatter.stringFromNumber(fourPeopleLabelBill)!
+    }
+    
+    func updateTipDisplay() {
+        tipControl.setTitle("\(Int(defaults.floatForKey("lowest_tip") * 100))%", forSegmentAtIndex: 0)
+        tipControl.setTitle("\(Int(defaults.floatForKey("mid_tip") * 100))%", forSegmentAtIndex: 1)
+        tipControl.setTitle("\(Int(defaults.floatForKey("highest_tip") * 100))%", forSegmentAtIndex: 2)
+        tipControl.selectedSegmentIndex = defaults.integerForKey("default_index")
+    }
+
 }
